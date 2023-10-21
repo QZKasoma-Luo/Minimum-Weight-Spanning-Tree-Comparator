8d8d211bdff3a1cf87c672c0d0754a03cac3ee9f
QingZe Luo <98981724+QZKasoma-Luo@users.noreply.github.com>
2023-10-20 19:15:16 -0700

    reset the file

--- PrimVsKruskal.java
+++ PrimVsKruskal.java
@@ -31,77 +31,12 @@
 */
 
  import edu.princeton.cs.algs4.*;
-
-import java.util.HashSet;
-import java.util.Scanner;
+ import java.util.Scanner;
  import java.io.File;
 
 //Do not change the name of the PrimVsKruskal class
 public class PrimVsKruskal{
 
-	//Add and inital the variables from algs4.jar, some of the variables and comments are from alg4.jar
-	private EdgeWeightedGraph EW_Graph; //transform the input txt file to a edge weighted graph
-	private Queue<Edge> mst = new Queue<>();
-	private IndexMinPQ<Double> pq; //eligible crossing edges
-	private boolean[] marked; //true if v in MST
-	private Edge[] edgeTo;        // edgeTo[v] = shortest edge from tree vertex to non-tree vertex
-    private double[] distTo;      // distTo[v] = weight of shortest such edge
-
-	//Transfor the input txt file to a edge weighted graph variable to be able to pass to the eagerPrim and Kruskal function
-	public void parameterTransfrom(double[][] G){
-		this.EW_Graph = new EdgeWeightedGraph(G.length);
-		for(int i = 0; i < G.length; i++){
-			for(int j = 0; j < G.length; j++){
-				if(G[i][j] > 0.0){
-					Edge e = new Edge(i, j, G[i][j]);
-					EW_Graph.addEdge(e);
-				}
-			}
-		}
-		marked = new boolean[EW_Graph.V()];
-		pq = new IndexMinPQ<>(EW_Graph.V());
-	}
-	
-	public void eagerPrim(EdgeWeightedGraph G){
-		//initialize the edgeTo and distTo
-		edgeTo = new Edge[G.V()];
-		distTo = new double[G.V()];
-		for(int v = 0; v < G.V(); v++){
-			distTo[v] = Double.POSITIVE_INFINITY;
-		}
-		
-		//initialize the first vertex
-		distTo[0] = 0.0; //Assume G is connected
-		pq.insert(0,0.0);
-		while(!pq.isEmpty()){
-			visit(G, pq.delMin()); //repeatedly delete the min weight edge e = v-w from pq 
-		}
-	}
-
-	private void visit(EdgeWeightedGraph G, int v){
-		marked[v] = true;
-		for(Edge e:G.adj(v)){ // add v to tree; update data structures
-			int w = e.other(v); // for each edge e = v-w, add w to pq if not already in tree
-			if(marked[w]) continue;
-			if(e.weight()<distTo[w]){
-				edgeTo[w] = e; //add e to tree
-				distTo[w] = e.weight();
-				if(pq.contains(w)) pq.changeKey(w, distTo[w]); // update distance to w or insert distance to w
-				else pq.insert(w, distTo[w]);
-			}
-		}
-	}
-
-	public Iterable<Edge> edges(){ //create the MST
-		mst = new Queue<Edge>();
-		for (int v = 0; v < edgeTo.length; v++){
-			Edge e = edgeTo[v];
-			if(e != null){
-				mst.enqueue(e);
-			}
-		}
-		return mst;
-	}	
 	/* PrimVsKruskal(G)
 		Given an adjacency matrix for connected graph G, with no self-loops or parallel edges,
 		determine if the minimum spanning tree of G found by Prim's algorithm is equal to 
@@ -112,7 +47,6 @@ public class PrimVsKruskal{
 		value of G[i][j] gives the weight of the edge.
 		No entries of G will be negative.
 	*/
-
 	static boolean PrimVsKruskal(double[][] G){
 		int n = G.length;
 
@@ -121,45 +55,13 @@ public class PrimVsKruskal{
 		
 		/* ... Your code here ... */
 		
-		PrimVsKruskal pmTransfer = new PrimVsKruskal();
-		pmTransfer.parameterTransfrom(G);
-		pmTransfer.eagerPrim(pmTransfer.EW_Graph);
-		Iterable<Edge> pmMST = pmTransfer.edges();
-
-
-		EdgeWeightedGraph kruskalGraph = new EdgeWeightedGraph(n);
-		for(int i = 0; i < n; i++){
-			for(int j = i+1; j < n; j++){
-				if(G[i][j] > 0.0){
-					Edge e = new Edge(i, j, G[i][j]);
-					kruskalGraph.addEdge(e);
-				}
-			}
-		}
-
-		KruskalMST kruskal_MST = new KruskalMST(kruskalGraph);
-		Iterable<Edge> kruskalMST = kruskal_MST.edges();
+		
 		/* Determine if the MST by Prim equals the MST by Kruskal */
 		boolean pvk = true;
 		/* ... Your code here ... */
 
-		HashSet<Edge> pmSet = new HashSet<>();
-		for(Edge e: pmMST){
-			pmSet.add(e);
-		}
-
-		HashSet<Edge> kruskalSet = new HashSet<>();
-		for(Edge e: kruskalMST){
-			kruskalSet.add(e);
-		}
-
-		if(!pmSet.equals(kruskalSet)){
-			pvk = false;
-		}
-
 		return pvk;	
 	}
-	
 		
 	/* main()
 	   Contains code to test the PrimVsKruskal function. You may modify the
